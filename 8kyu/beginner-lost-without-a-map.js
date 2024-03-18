778fe272f5b8600de62005041e6484c9ea82c237
polozovaanastasia <polozova.anastasia@gmail.com>
2024-03-18 21:39:54 +0500

    beginner-lost-without-a-map.

---
+++ 8kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// SOLUTION:
+
+maps = x => x.map(e => e * 2);
