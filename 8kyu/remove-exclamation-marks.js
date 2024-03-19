35d8f9306c5ad52f4dbba5db098122be154b9a2c
polozovaanastasia <polozova.anastasia@gmail.com>
2024-03-19 13:04:32 +0500

    remove-exclamation-marks.

---
+++ 8kyu/remove-exclamation-marks.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+// SOLUTION:
+const removeExclamationMarks = str => str.replace(/!/g,"");
