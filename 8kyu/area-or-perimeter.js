126075ec5fb21cc5bf5577c9fd2b48286986c1ed
polozovaanastasia <polozova.anastasia@gmail.com>
2024-03-20 15:03:41 +0500

    деталь.

--- 8kyu/area-or-perimeter.js
+++ 8kyu/area-or-perimeter.js
@@ -9,4 +9,4 @@
 // Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 
 // SOLUTION:
-const areaOrPerimeter = (l , w) => l === w ? l * w : l * 2 + w * 2;
+const areaOrPerimeter = (l , w) => l === w ? l * w : (l + w) * 2;
