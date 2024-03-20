4f9fcc41c4d261fcc272c254630855702865631b
polozovaanastasia <polozova.anastasia@gmail.com>
2024-03-20 15:06:58 +0500

    convert-boolean.

---
+++ 8kyu/convert-boolean.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// SOLUTION:
+const boolToWord = bool => bool ? 'Yes' : 'No';
